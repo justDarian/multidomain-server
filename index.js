ca7223baebc669111217f86d63df84c9c7641b8d
Darian <116446349+justDarian@users.noreply.github.com>
2024-02-29 17:51:08 -0500

    comments to make me giggle

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// created by darian, and the express documentation
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const endpoints = require('./routes.js');
+const endpoints = require('./routes.js'); // ./ :fire:
 const port = 80;
 
-app.enable('trust proxy'); // yes
-app.use(cors()); // oh yea!!
+app.enable('trust proxy'); // wtf?? this was just in express example
+app.use(cors()); // this carried production ong, i kept getting cors errors :sob:
 
 // make endpoint for the array in routes.js
 endpoints.forEach(({ domain, endpoint, callback }) => {
@@ -24,16 +26,17 @@ endpoints.forEach(({ domain, endpoint, callback }) => {
 // send stuff in the domain folder
 app.use((req, res, next) => {
   fs.appendFileSync(path.join(__dirname, "logs.txt"),`${(new Date().toLocaleString({hour12: true}))}   ${req.hostname}${req.url} - ${req.headers['x-forwarded-for'] || req.connection.remoteAddress} \n`);
-
+  // yes, im being professional!!
   const folderPath = path.join(__dirname, 'sites', req.hostname);
 
   if (fs.existsSync(folderPath)) {
     const filePath = path.join(folderPath, req.url);
 
+    // checks if the file even exists
     if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
       res.sendFile(filePath);
     } else {
-      express.static(folderPath, { fallthrough: false })(req, res, (err) => {
+      express.static(folderPath, { fallthrough: false })(req, res, (err) => { // ok bro the docs carried, i did NOT know failthrough was a thing
         if (err) {
           handleNotFound(req, res);
         }
@@ -44,13 +47,17 @@ app.use((req, res, next) => {
   }
 });
 
+// making this a function becuase i said so
 function handleNotFound(req, res) {
+  // wtf do i name this shit
+  // (also, you can chane this)
   const notFoundPath = path.join(path.join(__dirname, 'sites'), '404.html');
   fs.exists(notFoundPath, (exists) => {
     if (exists) {
+      // make sure the file doesnt include shit people can exploit, yk??
       res.status(404).sendFile(notFoundPath);
     } else {
-      res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+      res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ") // haha get trolled!!! (i spent 4 hours trying to figure out why req.redirect wasnt working, send help)
     }
   });
 }
