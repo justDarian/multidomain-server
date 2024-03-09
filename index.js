8a8bd108d8b2b3aa86d9ad78be6a27ef0fa59de5
Darian <116446349+justDarian@users.noreply.github.com>
2024-03-08 21:41:59 -0500

    fuck trust proxy (respectfully ofc)

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 const endpoints = require('./routes.js'); // ./ :fire:
 const port = 80;
 
-app.enable('trust proxy'); // wtf?? this was just in express example
 app.use(express.json()); // never forget express.json
 app.use(cors()); // this carried production ong, i kept getting cors errors :sob:
 
